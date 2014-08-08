5473a97f9b8cac2f22ca9a053e5cacd5c5416601
Michał Jezierski <ambinanitelo@gmail.com>
2014-08-08 09:32:42 +0200

    allow swipedown through page

--- src/jq-swipe.js
+++ src/jq-swipe.js
@@ -41,6 +41,10 @@
           deltaY = (Dy > 0) ? 1 : -1;
         }
 
+        if (deltaX !== 0) {
+          e.preventDefault();
+        }
+
         event.type = "swipe";
         args.unshift(event, deltaX, deltaY); // add back the new event to the front of the arguments with the delatas
         return ($.event.dispatch || $.event.handle).apply(that, args);
