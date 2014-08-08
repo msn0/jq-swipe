87f73db9d8bcd9fe40c7f4b6eaa10a1dc1919311
Michał Jezierski <ambinanitelo@gmail.com>
2014-08-08 09:39:54 +0200

    grunt-uglify task

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,10 +6,23 @@ module.exports = function (grunt) {
         jshintrc: ".jshintrc"
       },
       all: ['Gruntfile.js', 'src/**/*.js']
+    },
+    uglify: {
+      article: {
+        options: {
+          mangle: true
+        },
+        files: {
+          'dist/jq-swipe.min.js': [
+            'src/jq-swipe.js'
+          ]
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask("default", ['jshint']);
+  grunt.registerTask("default", ['jshint', 'uglify']);
 };
